c88790f2abae121760e5257c638ff92e450ece02
babjivoonna <babjivoonna189@gmail.com>
2024-04-24 20:04:36 +0530

    changecommited

--- src/assets/projectsData.js
+++ src/assets/projectsData.js
@@ -13,6 +13,7 @@ import SwordCustomize from '../assets/Sword-Customize.png'
 
 export const projectsData=[
     {id: 1, title: 'Food',      link:"https://food-order-website-beta.vercel.app/",  category: 'React Web Applications', image: FoodDelivery},
+    {id:10, title:'Bussiness', link:"https://sword-customizing.vercel.app/",category:"Full Stack Applications", image:SwordCustomize},
     {id: 3, title: 'College',   link:"https://college-website-one-tau.vercel.app/",  category: 'Web design', image: CollegeWebsite},
     {id: 4, title: 'ECommerce', link:"https://e-commerce-website-tau-two.vercel.app/",  category: 'React application', image:   Ecommercee },
     {id: 2, title: 'GymManage', link:"https://my-gym-nu.vercel.app/",  category: 'Full Stack Applications', image: GymMangement},
@@ -21,5 +22,4 @@ export const projectsData=[
     {id: 7, title: 'game',      link:"https://tic-tac-toe-three-beta-30.vercel.app/",  category: 'React Web Applications', image: TicTacToe},
     {id: 8, title: 'videoapp',  link:"https://video-tube-mu.vercel.app/",  category: 'Full Stack Applications', image: VideoTube},
     {id: 9, title: 'webSite',   link:"https://coursesite-11.vercel.app/",  category: 'Web design', image: CourseWebsite}, 
-    {id:10, title:'Bussiness', link:"https://sword-customizing.vercel.app/",category:"Full Stack Applications", image:SwordCustomize}
 ]
