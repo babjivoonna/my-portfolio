29caef58b1ec68b76b8cb318dc997ca530014620
babjivoonna <babjivoonna189@gmail.com>
2024-04-26 23:21:06 +0530

    change-added

--- src/assets/projectsData.js
+++ src/assets/projectsData.js
@@ -8,12 +8,15 @@ import TicTacToe from '../assets/TicTacToe.png'
 import VideoTube from '../assets/VideoTube.png'
 import CourseWebsite from '../assets/CourseWebsite.png'
 import SwordCustomize from '../assets/Sword-Customize.png'
+import KanbanDashboard from '../assets/KanbanDashboard.png'
 // import FoodDelivery from '../assets/FoodDelivery.png'
 
 
 export const projectsData=[
     {id: 1, title: 'Food',      link:"https://food-order-website-beta.vercel.app/",  category: 'React Web Applications', image: FoodDelivery},
     {id:10, title:'Bussiness', link:"https://sword-customizing.vercel.app/",category:"Full Stack Applications", image:SwordCustomize},
+    {id:11, title:'Work', link:"https://kanban-dashboard-ochre.vercel.app/",category:"React Web Applications", image:KanbanDashboard},
+
     {id: 3, title: 'College',   link:"https://college-website-one-tau.vercel.app/",  category: 'Web design', image: CollegeWebsite},
     {id: 4, title: 'ECommerce', link:"https://e-commerce-website-tau-two.vercel.app/",  category: 'React application', image:   Ecommercee },
     {id: 2, title: 'GymManage', link:"https://my-gym-nu.vercel.app/",  category: 'Full Stack Applications', image: GymMangement},
